ebdbe86d966d04be69ab462ff8c1970c0e031cb0
“zusquang” <zusquang@gmail.com>
2022-02-14 18:14:14 +0700

    update[chart]

--- components/common/Chart/ConversionRateChart.tsx
+++ components/common/Chart/ConversionRateChart.tsx
@@ -1,10 +1,10 @@
 
-import React, { useLayoutEffect, useState } from 'react'
+import React from 'react'
 import * as am4core from "@amcharts/amcharts4/core"
 import * as am4charts from "@amcharts/amcharts4/charts"
 import am4themes_animated from "@amcharts/amcharts4/themes/animated"
-import { ConversionRateAPI } from '@/apis'
 import { ConversionRate } from '@/types'
+import { useAmcharts } from '@/hooks/useAmcharts'
 
 am4core.useTheme(am4themes_animated)
 
@@ -12,6 +12,7 @@ export type ConversionRateChartProps = {
   firstColumn: ChartInfo
   secondColumn: ChartInfo
   lineChart: ChartInfo
+  data: ConversionRate[] | []
 }
 
 export type ChartInfo = {
@@ -20,107 +21,84 @@ export type ChartInfo = {
   color: string
 }
 
-const ConversionRateChart = ({ firstColumn, secondColumn, lineChart }: ConversionRateChartProps) => {
-  const [data, setData] = useState<ConversionRate[]>([])
-  
-  const getListData = async () => {
-    const data = await ConversionRateAPI.list()
-    setData(data)
-  }
+const ConversionRateChart = ({ firstColumn, secondColumn, lineChart, data }: ConversionRateChartProps) => {
 
-  useLayoutEffect(() => {
-    getListData()
-  })
+    const { amchartsElRef } = useAmcharts(am4charts.XYChart, (chart) => {
+      chart.data = data
+      chart.colors.step = 2
+      
+      chart.legend = new am4charts.Legend()
+      chart.legend.position = "top"
+      chart.legend.paddingBottom = 20
+      chart.legend.markers.template.width = 15
+      chart.legend.markers.template.height = 15
+      chart.legend.paddingRight = 1200
 
-  useLayoutEffect(() => {
-    createChart()
-  }, [data])
+      chart.events.on("beforedatavalidated", function() {
+        for(let i = 0; i < chart.data.length; i++) {
+          chart.data[i] = {...chart.data[i], year_month: `${chart.data[i].year}/${chart.data[i].month}`}
+        }
+      });
 
-  const createChart = () => {
-    const chart = am4core.create('chartdiv', am4charts.XYChart)
-    chart.data = data
-    chart.colors.step = 2
-    
-    chart.legend = new am4charts.Legend()
-    chart.legend.position = "top"
-    chart.legend.paddingBottom = 20
-    chart.legend.markers.template.width = 15
-    chart.legend.markers.template.height = 15
-    chart.legend.paddingRight = 1330
+      const categoryField =  "year_month"
+      
+      const xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+      xAxis.dataFields.category = categoryField
 
-    chart.events.on("beforedatavalidated", function() {
-      for(let i = 0; i < chart.data.length; i++) {
-        chart.data[i].year += "/" + chart.data[i].month
+      xAxis.renderer.cellStartLocation = 0.2
+      xAxis.renderer.cellEndLocation = 0.8
+      xAxis.renderer.grid.template.location = 0
+      xAxis.renderer.grid.template.disabled = true
+      
+      const yAxis = chart.yAxes.push(new am4charts.ValueAxis())
+      yAxis.min = 0
+      yAxis.extraMax = 0.1
+
+      const lineYAxis = chart.yAxes.push(new am4charts.ValueAxis())
+      lineYAxis.renderer.opposite = true
+      lineYAxis.min = 0
+      lineYAxis.max = 100
+      lineYAxis.renderer.labels.template.adapter.add("text", (value) => {
+          return value + ".0%"
+      })
+
+      const createSeries = (name: string, title: string, color: string) => {
+          let series = chart.series.push(new am4charts.ColumnSeries())
+          
+          series.dataFields.valueY = name
+          series.dataFields.categoryX = categoryField
+          series.yAxis = yAxis
+          series.name = title
+          series.fill = am4core.color(color)
+          series.stroke = am4core.color(color)
+          series.columns.template.strokeOpacity = 0
+          series.columns.template.column.cornerRadiusTopRight = 5
+          series.columns.template.column.cornerRadiusTopLeft = 5
+      
+          let bullet = series.bullets.push(new am4charts.LabelBullet())
+          bullet.interactionsEnabled = false
+          bullet.dy = -10
+          bullet.label.text = "{values.valueY.workingValue}"
+          bullet.label.fill = am4core.color(color)
       }
+      
+      createSeries(firstColumn.name, firstColumn.title, firstColumn.color)
+      createSeries(secondColumn.name, secondColumn.title, secondColumn.color)
+
+      const lineSeries = chart.series.push(new am4charts.LineSeries())
+      lineSeries.name = lineChart.title
+      lineSeries.dataFields.valueY = lineChart.name
+      lineSeries.dataFields.categoryX = categoryField
+      lineSeries.yAxis = lineYAxis
+      lineSeries.stroke = am4core.color(lineChart.color)
+      lineSeries.strokeWidth = 2
+
+      const bullet = lineSeries.bullets.push(new am4charts.LabelBullet())
+      bullet.interactionsEnabled = false
+      bullet.dy = -10
+      bullet.label.text = "{values.valueY.workingValue}%"
     })
 
-    const categoryField =  "year"
-    
-    const xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
-    xAxis.dataFields.category = categoryField
-    
-    xAxis.renderer.labels.template.adapter.add("textOutput", function(text) {
-      return text
-    })
-
-    xAxis.renderer.cellStartLocation = 0.2
-    xAxis.renderer.cellEndLocation = 0.8
-    xAxis.renderer.grid.template.location = 0
-    xAxis.renderer.grid.template.disabled = true
-    
-    const yAxis = chart.yAxes.push(new am4charts.ValueAxis())
-    yAxis.min = 0
-    yAxis.extraMax = 0.1
-
-    const lineYAxis = chart.yAxes.push(new am4charts.ValueAxis())
-    lineYAxis.renderer.opposite = true
-    lineYAxis.min = 0
-    lineYAxis.max = 100
-    lineYAxis.renderer.labels.template.adapter.add("text", (value) => {
-        return value + ".0%"
-    })
-
-    const createSeries = (name: string, color: string) => {
-        let series = chart.series.push(new am4charts.ColumnSeries())
-        
-        series.dataFields.valueY = name
-        series.dataFields.categoryX = categoryField
-        series.yAxis = yAxis
-        series.name = name
-        series.fill = am4core.color(color)
-        series.stroke = am4core.color(color)
-        series.columns.template.strokeOpacity = 0
-        series.columns.template.column.cornerRadiusTopRight = 5
-        series.columns.template.column.cornerRadiusTopLeft = 5
-    
-        let bullet = series.bullets.push(new am4charts.LabelBullet())
-        bullet.interactionsEnabled = false
-        bullet.dy = -10
-        bullet.label.text = "{values.valueY.workingValue}"
-        bullet.label.fill = am4core.color(color)
-    
-        return series
-    }
-    
-    createSeries(firstColumn.name, firstColumn.color)
-    createSeries(secondColumn.name, secondColumn.color)
-
-    const lineSeries = chart.series.push(new am4charts.LineSeries())
-    lineSeries.name = lineChart.title
-    lineSeries.dataFields.valueY = lineChart.name
-    lineSeries.dataFields.categoryX = categoryField
-    lineSeries.yAxis = lineYAxis
-    lineSeries.stroke = am4core.color(lineChart.color)
-    lineSeries.strokeWidth = 2
-
-    const bullet = lineSeries.bullets.push(new am4charts.LabelBullet())
-    bullet.interactionsEnabled = false
-    bullet.dy = -10
-    bullet.label.text = "{values.valueY.workingValue}%"
-  }
-  
-  return (
-    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
-  )
+    return <div ref={amchartsElRef} style={{ width: "100%", height: "500px" }}/>;
 }
 export default ConversionRateChart
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,15 +2,15 @@ import { Layout } from '@/components';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import dynamic from 'next/dynamic'
 
-// const DynamicComponent = dynamic(() => import('@/components/common/Chart/ConversionRateChart'), {
-//   ssr: false
-// })
+const DynamicComponent = dynamic(() => import('@/components/common/Chart/ConversionRateChart'), {
+  ssr: false
+})
 
 function Dashboard() {
   return (
     <Layout title='menuDashboard'>
       <div className='flex'>dashboard</div>
-      {/* <DynamicComponent firstColumn={{name:"f1_uu", title: "F1", color: "#ffba00"}} secondColumn={{name:"f2_uu", title: "F2", color: "#55c5d9"}}
+      {/* <DynamicComponent data=[] firstColumn={{name:"f1_uu", title: "F1", color: "#ffba00"}} secondColumn={{name:"f2_uu", title: "F2", color: "#55c5d9"}}
       lineChart={{name:"f2_conversion_rate", title: "F2 conversion rate", color: "#ff7d58"}}/> */}
     </Layout>
   );
